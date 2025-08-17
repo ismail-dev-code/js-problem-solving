199dbff63f5d186f8d20313335c7736e31f3efa1
ismail-dev-code <hm.ismail772@gmail.com>
2025-08-17 23:33:40 +0600

    count vowels in a string problem solved

--- index.js
+++ index.js
@@ -2,4 +2,15 @@
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
-console.log(reverseString("hello")); 
+// console.log(reverseString("hello")); 
+
+// Problem 2: Count Vowels in a String
+function countVowels(str) {
+  let vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let char of str) {
+    if (vowels.includes(char)) count++;
+  }
+  return count;
+}
+console.log(countVowels("ismail"));
